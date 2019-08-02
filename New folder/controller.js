82ef1bd7bc554da1787c714e3b14395535868668
prathapkabali <47860248+prathap17017@users.noreply.github.com>
2019-08-02 14:10:02 +0530

    Add files via upload

---
+++ New folder/controller.js
@@ -0,0 +1,9 @@
+ <script>
+         function studentController($scope,$http) {
+            var url = "/data.txt";
+
+            $http.get(url).then( function(response) {
+               $scope.students = response.data;
+            });
+         }
+      </script>
